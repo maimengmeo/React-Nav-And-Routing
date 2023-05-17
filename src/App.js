0f0aebfe5bfe72c54081a1ce7046441312f9a031
maimengmeo <mai291294@gmail.com>
2023-05-16 21:09:09 -0400

    added react icon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import Button from "./Button";
+import { GoBell, GoCloudDownload } from "react-icons/go";
 
 function App() {
     return (
         <div>
             <div>
                 <Button primary rounded>
+                    <GoBell />
                     Click Me!
                 </Button>
             </div>
             <div>
-                <Button primary>Buy Now!</Button>
+                <Button primary>
+                    <GoCloudDownload />
+                    Buy Now!
+                </Button>
             </div>
             <div>
                 <Button success outline>
--- src/Button.js
+++ src/Button.js
@@ -10,7 +10,7 @@ function Button({
     rounded,
     outline,
 }) {
-    const classes = className("px-3 py-1.5 border", {
+    const classes = className("flex items-center px-3 py-1.5 border", {
         "border-blue-500 bg-blue-500 text-white": primary,
         "border-gray-500 bg-gray-500 text-white": secondary,
         "border-green-500 bg-green-500 text-white": success,
