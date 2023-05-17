33dd13ad3054fa7894be09f22ca6ebe207949baf
maimengmeo <mai291294@gmail.com>
2023-05-16 22:21:11 -0400

    handled special classname case

--- src/Button.js
+++ src/Button.js
@@ -13,7 +13,7 @@ function Button({
     ...rest //this will asign other props to the button like onClick, onMouseOver
 }) {
     const classes = twMerge(
-        className("flex items-center px-3 py-1.5 border", {
+        className(rest.className, "flex items-center px-3 py-1.5 border", {
             "border-blue-500 bg-blue-500 text-white": primary,
             "border-gray-500 bg-gray-500 text-white": secondary,
             "border-green-500 bg-green-500 text-white": success,
