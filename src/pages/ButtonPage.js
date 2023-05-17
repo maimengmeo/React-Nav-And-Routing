316a64d8ff4f0f4b06208006b6c1bec4ee289f39
maimengmeo <mai291294@gmail.com>
2023-05-17 15:29:05 -0400

    refactored with organization

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -1,7 +1,7 @@
-import Button from "./components/Button";
+import Button from "../components/Button";
 import { GoBell, GoCloudDownload } from "react-icons/go";
 
-function App() {
+function ButtonPage() {
     const handleClick = () => {
         console.log("clicked");
     };
@@ -43,4 +43,4 @@ function App() {
     );
 }
 
-export default App;
+export default ButtonPage;
