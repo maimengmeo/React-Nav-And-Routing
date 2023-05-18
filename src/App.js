5726fcaf4e0cc906758550e8b408b021cb849159
maimengmeo <mai291294@gmail.com>
2023-05-17 22:40:06 -0400

    rendered dropdown list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Dropdown from "./components/DropDown";
 
 function App() {
+    const options = [
+        { label: "Red", value: "red" },
+        { label: "Blue", value: "blue" },
+        { label: "Green", value: "green" },
+    ];
+
     //return <ButtonPage />;
     //return <Accordion items={items} />;
-    return <Dropdown />;
+    return <Dropdown options={options} />;
 }
 
 export default App;
