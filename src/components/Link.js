54ac0c0515143275ddcc1295e8f7b94a4675a0ec
maimengmeo <mai291294@gmail.com>
2023-05-18 22:19:58 -0400

    hanlder cmd & ctrl key with link

--- src/components/Link.js
+++ src/components/Link.js
@@ -7,12 +7,21 @@ function Link({ to, children }) {
 
     //when user click a link, prevent default to prevent the auto rendering, then navigate user by the function
     const handleClick = (event) => {
+        //if user hold ctrl/cmd key, treat the link as usual (normal link), will open link in new tab
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }
+
         event.preventDefault();
 
         navigate(to);
     };
 
-    return <a onClick={handleClick}>{children}</a>;
+    return (
+        <a href={to} onClick={handleClick}>
+            {children}
+        </a>
+    );
 }
 
 export default Link;
