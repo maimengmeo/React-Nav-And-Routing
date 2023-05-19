261a5af5bb261eb089bda62110b15b58e649dfbd
maimengmeo <mai291294@gmail.com>
2023-05-18 23:29:06 -0400

    implemented toggling visibility

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,10 +1,24 @@
 import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
 
 function ModalPage() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClick = () => {
+        setShowModal(true);
+    };
+
     return (
         <div>
-            <Modal />
+            <Button onClick={handleClick} primary>
+                Open Modal
+            </Button>
+            {showModal && <Modal />}
         </div>
     );
 }
 export default ModalPage;
+
+// parent owns state to keep track of whether modal should be displayed
+// parent component can show a modal at any time for any reason, not only when clicking the button
