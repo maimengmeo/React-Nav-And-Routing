32d7fca1383ebfe42a505c336ec82c37a098591a
maimengmeo <mai291294@gmail.com>
2023-05-18 20:27:47 -0400

    added navigation context

---
+++ src/context/Navigation.js
@@ -0,0 +1,14 @@
+import { Children, createContext } from "react";
+
+const NavigationContext = createContext();
+
+const NavigationProvider = ({ children }) => {
+    return (
+        <NavigationContext.Provider value={{}}>
+            {children}
+        </NavigationContext.Provider>
+    );
+};
+
+export { NavigationProvider };
+export default NavigationContext;
