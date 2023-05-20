63df936fe348ee6afb6611ac9407bc9ad7c0ab09
maimengmeo <mai291294@gmail.com>
2023-05-19 22:42:18 -0400

    updated modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,26 @@
 import ReactDOM from "react-dom";
+import { useEffect } from "react";
 
 function Modal({ onClose, children, actionBar }) {
+    //when the model is showed, user wont be able to scroll
+    useEffect(() => {
+        document.body.classList.add("overflow-hidden");
+
+        return () => {
+            //clean up function
+            document.body.classList.remove("overflow-hidden");
+        };
+    }, []);
+
     //with this create portal, the modal is sent to position that we want which is the div inside of body tag in index page.
     //so it wont be affected by the other positioned parent which make absolute wont work as expected
     return ReactDOM.createPortal(
         <div>
             <div
-                className="absolute inset-0 bg-gray-300 opacity-80"
+                className="fixed inset-0 bg-gray-300 opacity-80"
                 onClick={onClose}
             ></div>
-            <div className="absolute inset-40 p-10 bg-white">
+            <div className="fixed inset-40 p-10 bg-white">
                 <div className="flex flex-col justify-between h-full">
                     {children}
                     <div className="flex justify-end">{actionBar}</div>
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -34,6 +34,53 @@ function ModalPage() {
                 Open Modal
             </Button>
             {showModal && modal}
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                feugiat eu nisl sit amet blandit. Phasellus gravida libero ut
+                ornare ullamcorper. Donec ut lacus vitae felis tincidunt
+                elementum eu sit amet felis. Mauris dictum, quam ut vehicula
+                accumsan, metus augue laoreet dui, vitae rutrum massa enim sit
+                amet nisi. Vestibulum iaculis purus at fringilla semper. Duis
+                rhoncus nisl sed massa elementum venenatis. Praesent tortor
+                diam, venenatis ac mi eget, vehicula pretium sapien. Nam
+                laoreet, ligula in rhoncus porta, tortor libero eleifend est,
+                sed facilisis ligula nibh in sem. Pellentesque sit amet velit
+                sed massa ullamcorper dapibus eget eget felis. Phasellus
+                fringilla tellus ac dolor finibus pharetra. Proin lectus purus,
+                sodales et elit ut, consequat aliquet nunc. Donec porttitor id
+                dui elementum eleifend. Aenean quis vehicula libero. Duis in dui
+                aliquam, rhoncus odio et, condimentum nibh. Duis in scelerisque
+                sem, et sagittis eros. Pellentesque id lectus et odio dictum
+                consectetur vitae vitae turpis. Mauris in metus neque.
+                Suspendisse consequat porttitor felis quis tincidunt.
+                Suspendisse iaculis porttitor lacinia. Donec fringilla orci nec
+                nunc posuere semper. Nunc dignissim dictum metus, ultrices
+                condimentum purus ultrices eu. Mauris efficitur consequat enim
+                ac congue. Integer fermentum, sem ut congue eleifend, risus est
+                rhoncus risus, ut pellentesque dui dui eu diam. Vestibulum
+                accumsan, nisi vitae viverra aliquet, quam nulla iaculis dolor,
+                eget ultricies nibh turpis sed justo. Cras vitae augue
+                scelerisque, sollicitudin odio eu, tempus tortor. Duis iaculis
+                vitae quam quis maximus. Integer nec fermentum lectus. Phasellus
+                vitae maximus eros. Vestibulum viverra eu ante eu venenatis.
+                Donec sed laoreet dolor. Nulla eu finibus velit. Sed sed
+                molestie risus. Donec ac odio at felis aliquam semper a quis
+                dui. Integer accumsan neque sit amet venenatis lacinia. Aliquam
+                at varius metus, in fermentum nisl. Cras eleifend diam in neque
+                fringilla, nec fringilla sapien efficitur. Nullam consequat ex
+                ut tincidunt porta. Vestibulum at enim in odio ullamcorper
+                egestas. Aenean arcu elit, tempus a lorem ut, pretium mollis
+                quam. Curabitur egestas eleifend porta. Vivamus vel dignissim
+                orci. Nunc quis dignissim ipsum. Vivamus volutpat erat quis quam
+                feugiat, ut porta nisl sagittis. Vestibulum laoreet mi ex, sed
+                accumsan elit congue sed. Mauris fermentum tristique semper.
+                Proin lacinia at justo consequat tincidunt. Donec eleifend
+                pulvinar mattis. Aenean interdum tellus vitae ex hendrerit, sed
+                pretium diam sagittis. Donec iaculis, arcu at lacinia iaculis,
+                diam elit vestibulum purus, sit amet tincidunt quam mi quis ex.
+                Maecenas id metus in quam iaculis varius at quis neque.
+            </p>
         </div>
     );
 }
