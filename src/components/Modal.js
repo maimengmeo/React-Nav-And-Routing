8f80e00deebaef034af254791eedf1c747c4b299
maimengmeo <mai291294@gmail.com>
2023-05-19 22:16:55 -0400

    fixed the modal with portal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,14 @@
+import ReactDOM from "react-dom";
+
 function Modal() {
-    return (
+    //with this create portal, the modal is sent to position that we want which is the div inside of body tag in index page.
+    //so it wont be affected by the other positioned parent which make absolute wont work as expected
+    return ReactDOM.createPortal(
         <div>
             <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
             <div className="absolute inset-40 p-10 bg-white">Im a Modal</div>
-        </div>
+        </div>,
+        document.querySelector("modal-container")
     );
 }
 
