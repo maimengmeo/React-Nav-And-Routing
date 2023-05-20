bba31818511e1ff6854c6474576a7827c44a13fa
maimengmeo <mai291294@gmail.com>
2023-05-19 22:22:10 -0400

    handled close modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,17 @@
 import ReactDOM from "react-dom";
 
-function Modal() {
+function Modal({ onClose }) {
     //with this create portal, the modal is sent to position that we want which is the div inside of body tag in index page.
     //so it wont be affected by the other positioned parent which make absolute wont work as expected
     return ReactDOM.createPortal(
         <div>
-            <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+            <div
+                className="absolute inset-0 bg-gray-300 opacity-80"
+                onClick={onClose}
+            ></div>
             <div className="absolute inset-40 p-10 bg-white">Im a Modal</div>
         </div>,
-        document.querySelector("modal-container")
+        document.querySelector(".modal-container")
     );
 }
 
