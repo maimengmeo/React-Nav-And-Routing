2328e65e1cf1e7082732c3db9a16f429e1f33731
maimengmeo <mai291294@gmail.com>
2023-05-23 14:52:15 -0400

    added table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,26 @@
-function Table() {
-    return <div>Table</div>;
+function Table({ data }) {
+    const renderedRow = data.map((d) => {
+        return (
+            <tr>
+                <td>{d.name}</td>
+                <td>{d.color}</td>
+                <td>{d.score}</td>
+            </tr>
+        );
+    });
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Fruit</th>
+                    <th>Color</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>{renderedRow}</tbody>
+        </table>
+    );
 }
 
 export default Table;
