07fd95542d2531afcd407b029febf6f62fa2eabd
maimengmeo <mai291294@gmail.com>
2023-05-23 14:57:03 -0400

    added styles

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,21 @@
 function Table({ data }) {
     const renderedRow = data.map((d) => {
         return (
-            <tr>
-                <td>{d.name}</td>
-                <td>{d.color}</td>
-                <td>{d.score}</td>
+            <tr key={d.name} className="border-b">
+                <td className="p-3">{d.name}</td>
+                <td className="p-3">
+                    <div className={`p-3 m-2 ${d.color}`}></div>
+                    {/* make the colored square */}
+                </td>
+                <td className="p-3">{d.score}</td>
             </tr>
         );
     });
 
     return (
-        <table>
+        <table className="table-auto border-spacing-2">
             <thead>
-                <tr>
+                <tr className="boder-b-2">
                     <th>Fruit</th>
                     <th>Color</th>
                     <th>Score</th>
