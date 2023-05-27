7dd5cfa19fd4b2b3005025f6a3f4ac027b1070a8
maimengmeo <mai291294@gmail.com>
2023-05-27 15:27:49 -0400

    created sortable table

---
+++ src/components/SortableTable.js
@@ -0,0 +1,29 @@
+//create sortabletabel, take all props & pass then through Table
+//make sure tablepage shows sortabletable
+//add 'sortValue' func to column config in tablepage. rm header prop bc header will be handle in sortable table
+//sortabletable should find column objects with 'sortValue' & add a header func to them
+//th returned by the header func should watch for click event
+//when user clicks the th, sort data & pass the result to table
+//table->shortabletable->tablepage
+
+import Table from "./Table";
+
+function SortableTable(props) {
+    const { config } = props;
+
+    const updatedConfig = config.map((c) => {
+        if (!c.sortValue) {
+            return c;
+        }
+
+        //return an obj, if there is sort value, tranform obj by adding header func
+        return {
+            ...c,
+            header: () => <th>{c.label} IS SORTED</th>,
+        };
+    });
+
+    return <Table {...props} config={updatedConfig} />;
+}
+
+export default SortableTable;
