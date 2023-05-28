205045b0712f63a9dca6cf8b36549d43d7dfe237
maimengmeo <mai291294@gmail.com>
2023-05-27 20:53:45 -0400

    created reusable sorting hook

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -6,42 +6,13 @@
 //when user clicks the th, sort data & pass the result to table
 //table->shortabletable->tablepage
 
-//sortable table:
-//2 pcs of state:
-//  +track of the sort order: null 'asc' 'desc'
-//  +track which col is sorting: null, 'name', 'score'
-
 import Table from "./Table";
-import { useState } from "react";
+import useSort from "../hooks/use-sort";
 import { GoChevronDown, GoChevronUp } from "react-icons/go";
 
 function SortableTable(props) {
     const { config, data } = props;
-    const [sortOrder, setSortOrder] = useState(null);
-    const [sortBy, setSortBy] = useState(null);
-
-    //handle click will change the state. null-asc-desc-null
-    //everytime click, app will be rerendered, resort, passed to table
-    const handleClick = (label) => {
-        //check if the clicked label is same as the current label,
-        //if not reset the sortedData, get new sortedData by the new label
-        if (sortBy && label !== sortBy) {
-            setSortBy(label);
-            setSortOrder("asc");
-            return;
-        }
-
-        if (sortOrder === null) {
-            setSortOrder("asc");
-            setSortBy(label);
-        } else if (sortOrder === "asc") {
-            setSortOrder("desc");
-            setSortBy(label);
-        } else if (sortOrder === "desc") {
-            setSortOrder(null);
-            setSortBy(label);
-        }
-    };
+    const { sortOrder, sortBy, sortedData, setSortCol } = useSort(data, config);
 
     const updatedConfig = config.map((c) => {
         if (!c.sortValue) {
@@ -54,7 +25,7 @@ function SortableTable(props) {
             header: () => (
                 <th
                     className="cursor-pointer hover:bg-gray-100"
-                    onClick={() => handleClick(c.label)}
+                    onClick={() => setSortCol(c.label)}
                 >
                     <div className="flex items-center">
                         {getIcons(c.label, sortBy, sortOrder)}
@@ -64,25 +35,6 @@ function SortableTable(props) {
             ),
         };
     });
-
-    //only sort if sortOrder && sortBy !== null
-    //make a copy of the 'data' prop. never modify an array directly if that array is a part of the prop sys or state sys
-    //find the correct sortValue function & use it for sorting
-    let sortedData = [...data];
-    if (sortBy && sortOrder) {
-        const { sortValue } = config.find((c) => c.label === sortBy);
-        sortedData = sortedData.sort((a, b) => {
-            //comparetor function that work with sort func
-            const reverseOrder = sortOrder === "asc" ? 1 : -1; //if asc, keep the order, otherwise, return value will be * -1, order is revered to desc
-
-            const valueA = sortValue(a);
-            const valueB = sortValue(b);
-
-            if (typeof valueA === "string") {
-                return valueA.localeCompare(valueB) * reverseOrder;
-            } else return (valueA - valueB) * reverseOrder;
-        });
-    }
 
     return (
         <div>
